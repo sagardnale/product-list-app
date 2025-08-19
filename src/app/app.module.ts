a165ec52c224a7eea5a61004da65dee8a3dee925
sagardnale <nalesagard@gmail.com>
2025-08-19 14:28:43 +0530

    created product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductsComponent } from './products/products.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
